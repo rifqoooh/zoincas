9518e8774ca1d8aaf7ce230d404d3215e6996f9f
arij rifqoh <arijrifqoh@gmail.com>
2025-07-11 21:34:32 +0700

    feat(app): add create edit budget plan hooks

--- src/hooks/actions/use-create-edit-budget-plan.ts
+++ src/hooks/actions/use-create-edit-budget-plan.ts
@@ -1,29 +1,46 @@
 'use client';
 
-import type { InsertBudgetPlansType } from '@/validators/db/budget-plans';
+import type {
+  InsertBudgetPlansFormType,
+  InsertBudgetPlansType,
+} from '@/validators/db/budget-plans';
 import type { SubmitHandler } from 'react-hook-form';
 
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useFieldArray, useForm } from 'react-hook-form';
+import { toast } from 'sonner';
 
-import { useGetBudgetPlanQuery } from '@/hooks/queries/budget-plans';
+import {
+  useCreateBudgetPlanMutation,
+  useGetBudgetPlanQuery,
+} from '@/hooks/queries/budget-plans';
 import { useCreateEditBudgetModal } from '@/hooks/store/create-edit-budget';
-import { insertBudgetPlansSchema } from '@/validators/db/budget-plans';
+import { useDeleteBudgetCategoryModal } from '@/hooks/store/delete-budget-category';
+import {
+  insertBudgetPlansFormSchema,
+  insertBudgetPlansSchema,
+} from '@/validators/db/budget-plans';
 
 export const useCreateEditBudgetPlan = () => {
-  const store = useCreateEditBudgetModal();
-  const isCreating = store.id === undefined;
+  const createEditStore = useCreateEditBudgetModal();
+  const isCreating = createEditStore.id === undefined;
 
-  //   const createMutation = useCreateBudgetPlanMutation();
-  //   const updateMutation = useUpdateBudgetPlanMutation(store.id);
+  const deleteStore = useDeleteBudgetCategoryModal();
 
-  const budgetPlanQuery = useGetBudgetPlanQuery(store.id);
+  const createMutation = useCreateBudgetPlanMutation();
+  //   const updateMutation = useUpdateBudgetPlanMutation(createEditStore.id);
+
+  const budgetPlanQuery = useGetBudgetPlanQuery(createEditStore.id);
 
   // fallback to default values budget plan data is undefined
-  const defaultValues: InsertBudgetPlansType = budgetPlanQuery.data
+  const defaultValues: InsertBudgetPlansFormType = budgetPlanQuery.data
     ? {
         title: budgetPlanQuery.data.title || '',
-        categories: budgetPlanQuery.data.categories,
+        categories: budgetPlanQuery.data.categories.map((category) => ({
+          categoryId: category.id,
+          name: category.name,
+          amount: category.amount,
+        })),
       }
     : {
         title: '',
@@ -35,8 +52,8 @@ export const useCreateEditBudgetPlan = () => {
         ],
       };
 
-  const form = useForm<InsertBudgetPlansType>({
-    resolver: zodResolver(insertBudgetPlansSchema),
+  const form = useForm<InsertBudgetPlansFormType>({
+    resolver: zodResolver(insertBudgetPlansFormSchema),
     defaultValues,
   });
 
@@ -45,10 +62,42 @@ export const useCreateEditBudgetPlan = () => {
     control: form.control,
   });
 
-  const onSubmit: SubmitHandler<InsertBudgetPlansType> = (values) => {
-    console.log(values);
+  const onCreateSubmit = (values: InsertBudgetPlansType) => {
+    return toast.promise(
+      createMutation.mutateAsync(values, {
+        onSuccess: () => {
+          createEditStore.onClose();
+        },
+      }),
+      {
+        loading: 'Creating budget plan...',
+        success: 'Budget plan created successfully',
+        error: (error: unknown) => {
+          if (error instanceof Error) {
+            return error.message;
+          }
+
+          return 'There is an error in the internal server.';
+        },
+      }
+    );
+  };
+
+  const onSubmit: SubmitHandler<InsertBudgetPlansFormType> = (values) => {
+    // mapped categories
+    values.categories = values.categories.map((category) => ({
+      id: category.categoryId,
+      name: category.name,
+      amount: category.amount,
+    }));
 
     const parsedData = insertBudgetPlansSchema.parse(values);
+
+    if (isCreating) {
+      onCreateSubmit(parsedData);
+    } else {
+      console.log(parsedData);
+    }
   };
 
   return {
@@ -56,5 +105,6 @@ export const useCreateEditBudgetPlan = () => {
     fieldArray,
     onSubmit,
     isCreating,
+    deleteStore,
   };
 };
