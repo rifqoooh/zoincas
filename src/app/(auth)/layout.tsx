ccc4365e87c2e2336fb5d767a25e065536f10587
arij rifqoh <arijrifqoh@gmail.com>
2025-07-15 12:36:13 +0700

    chore(ui): minor changes

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -8,10 +8,10 @@ interface AuthLayoutProps {
 
 export default function AuthLayout({ children }: AuthLayoutProps) {
   return (
-    <div className="container relative grid h-svh flex-col items-center justify-center lg:max-w-none lg:grid-cols-2 lg:px-0 xl:grid-cols-[45%_55%]">
+    <div className="relative grid h-svh flex-col items-center justify-center lg:max-w-none lg:grid-cols-2 lg:px-0 xl:grid-cols-[45%_55%]">
       <div>{children}</div>
 
-      <div className="relative hidden h-full bg-muted p-10 text-white lg:flex lg:flex-col dark:border-r">
+      <div className="relative hidden h-full p-10 text-primary-foreground lg:flex lg:flex-col dark:border-r">
         <Background />
       </div>
     </div>
