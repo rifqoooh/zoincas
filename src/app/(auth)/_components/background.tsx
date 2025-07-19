ebc0146ddc16de457fe5b0cec3a5beba33f91747
arij rifqoh <arijrifqoh@gmail.com>
2025-07-19 16:31:22 +0700

    chore(ui): update auth background

--- src/app/(auth)/_components/background.tsx
+++ src/app/(auth)/_components/background.tsx
@@ -1,13 +1,19 @@
+import { LiquidChrome } from '@/components/liquid-chrome';
+
 export function Background() {
   return (
     <>
-      <div className="absolute inset-0 bg-primary" />
-
-      <div className="relative z-20 flex items-center gap-2 font-medium text-lg">
-        zoincas
+      <div className="absolute inset-0">
+        <LiquidChrome
+          baseColor={[0.1, 0.1, 0.1]}
+          speed={0.3}
+          amplitude={0.5}
+          interactive={false}
+        />
       </div>
 
-      <div className="relative z-20 mt-auto">
+      <div className="relative">
+        <p className="font-medium text-lg">zoincas</p>
         <p className="text-sm">Rich mindset manage their money.</p>
       </div>
     </>
--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -11,7 +11,7 @@ export default function AuthLayout({ children }: AuthLayoutProps) {
     <div className="relative grid h-svh flex-col items-center justify-center lg:max-w-none lg:grid-cols-2 lg:px-0 xl:grid-cols-[45%_55%]">
       <div>{children}</div>
 
-      <div className="relative hidden h-full p-10 text-primary-foreground lg:flex lg:flex-col dark:border-r">
+      <div className="relative hidden h-full p-10 text-white lg:flex lg:flex-col">
         <Background />
       </div>
     </div>
