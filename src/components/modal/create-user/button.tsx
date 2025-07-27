60ea864d897c07e96d40c2ee625331cd7f185347
arij rifqoh <arijrifqoh@gmail.com>
2025-07-27 17:32:51 +0700

    fix(ui): minor fix

--- src/components/modal/create-user/button.tsx
+++ src/components/modal/create-user/button.tsx
@@ -6,8 +6,12 @@ import { useCreateUserModal } from '@/hooks/store/create-user';
 export function CreateUserButton() {
   const store = useCreateUserModal();
 
+  const onClick = () => {
+    store.onOpen();
+  };
+
   return (
-    <Button className="gap-2" size="sm" onClick={store.onOpen}>
+    <Button className="gap-2" size="sm" onClick={onClick}>
       <span>Create new user</span>
     </Button>
   );
