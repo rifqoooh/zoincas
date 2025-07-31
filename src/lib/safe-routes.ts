1378ffeb2ca6afcc6d80a8a8930be6a80bdfa54f
arij rifqoh <arijrifqoh@gmail.com>
2025-07-31 07:53:04 +0700

    chore(app): add all pages safe routes

--- src/lib/safe-routes.ts
+++ src/lib/safe-routes.ts
@@ -1,3 +1,5 @@
+import { selectTransactionsSchema } from '@/validators/db/transactions';
+import { zodEnumFromZodObject } from '@/validators/utilities';
 import { z } from 'zod';
 import { createRoute } from './create-route';
 
@@ -12,5 +14,35 @@ export const SignInSearchParams = z.object({
 export const Routes = {
   root: createRoute(() => '/', z.object({}), RootSearchParams),
   signIn: createRoute(() => '/sign-in', z.object({}), SignInSearchParams),
-  dashboard: createRoute(() => '/dashboard'),
+  signUp: createRoute(() => '/sign-up'),
+  dashboard: createRoute(
+    () => '/dashboard',
+    z.object({
+      startDate: z.coerce.number().optional(),
+      endDate: z.coerce.number().optional(),
+      balance: z.string().optional(),
+    })
+  ),
+  balances: createRoute(() => '/balances'),
+  categories: createRoute(() => '/categories'),
+  transactions: createRoute(
+    () => '/transactions',
+    z.object({
+      page: z.coerce.number().int().positive().optional(),
+      perPage: z.coerce.number().int().positive().optional(),
+      sort: z
+        .object({
+          id: zodEnumFromZodObject(selectTransactionsSchema),
+          desc: z.boolean(),
+        })
+        .array()
+        .optional(),
+      description: z.string().optional(),
+      balance: z.string().array().optional(),
+      category: z.string().array().optional(),
+      budget: z.string().array().optional(),
+      datetime: z.coerce.number().array().max(2).optional(),
+    })
+  ),
+  budgets: createRoute(() => '/budgets'),
 };
